0ebf723fa1047f9b438270800ccfd9afd7043308
npyl <n.pylarinos@hotmail.com>
2018-09-28 03:48:18 +0300

    Initial Commit

---
+++ NPTask/NPTask.h
@@ -0,0 +1,19 @@
+//
+//  NPTask.h
+//  NPTask
+//
+//  Created by Nickolas Pylarinos Stamatelatos on 28/09/2018.
+//  Copyright © 2018 Nickolas Pylarinos Stamatelatos. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for NPTask.
+FOUNDATION_EXPORT double NPTaskVersionNumber;
+
+//! Project version string for NPTask.
+FOUNDATION_EXPORT const unsigned char NPTaskVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NPTask/PublicHeader.h>
+
+
