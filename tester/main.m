bc881a6db5e647524bb387aa483f8e7c3d5ef090
npyl <n.pylarinos@hotmail.com>
2019-01-25 00:26:34 +0200

    Update main.m
    Add test for SESSIONS

--- tester/main.m
+++ tester/main.m
@@ -9,26 +9,27 @@
 #import <NPTask/NSAuthenticatedTask.h>   // include for launchAuthenticated addition
 
 int main(int argc, const char * argv[])
-{    
-    NSAuthenticatedTask *task = [[NSAuthenticatedTask alloc] init];
-
-    /*
-     * Allow us to call another script with admin
-     * privileges without having to type in the password again.
-     */
-    task.stayAuthorized = YES;
-    
-    // batch1
-    task.launchPath = @"/bin/mkdir";
-    task.arguments = @[@"/hello.1"];
-    [task launchAuthorized];
-    [task waitUntilExit];
-
-    // batch2
-    task.launchPath = @"/bin/mkdir";
-    task.arguments = @[@"/hello.2"];
-    [task launchAuthorized];
-    [task waitUntilExit];
+{
+// TEST1: stayAuthorized
+//    NSAuthenticatedTask *task = [[NSAuthenticatedTask alloc] init];
+//
+//    /*
+//     * Allow us to call another script with admin
+//     * privileges without having to type in the password again.
+//     */
+//    task.stayAuthorized = YES;
+//
+//    // batch1
+//    task.launchPath = @"/bin/mkdir";
+//    task.arguments = @[@"/hello.1"];
+//    [task launchAuthorized];
+//    [task waitUntilExit];
+//
+//    // batch2
+//    task.launchPath = @"/bin/mkdir";
+//    task.arguments = @[@"/hello.2"];
+//    [task launchAuthorized];
+//    [task waitUntilExit];
     
     /*
     NSFileHandle *fh = [[task standardOutput] fileHandleForReading];
@@ -37,5 +38,27 @@ int main(int argc, const char * argv[])
     NSLog(@"GOT: %@", str);
      */
     
+// TEST2: Assign to Pre-authorized Session
+    NSAuthenticatedTask *task2_1 = [[NSAuthenticatedTask alloc] init];
+
+    // batch1
+    task2_1.launchPath = @"/bin/mkdir";
+    task2_1.arguments = @[@"/hello.1"];
+    NSASession sessionA = [task2_1 launchAuthorized];
+    [task2_1 waitUntilExit];
+    
+    if (sessionA == -1)
+        NSLog(@"Oopsies: Something went wrong with launching task2_1");
+    else
+    {
+        NSAuthenticatedTask *task2_2 = [[NSAuthenticatedTask alloc] init];
+        
+        // batch2
+        task2_2.launchPath = @"/bin/mkdir";
+        task2_2.arguments = @[@"/hello.1"];
+        [task2_2 launchAuthorizedWithSession:sessionA];
+        [task2_2 waitUntilExit];
+    }
+
     return 0;
 }
